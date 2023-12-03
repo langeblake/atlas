f8acb2e99473f51f3f3d2ee8248d914594c5a860
langeblake <114412977+langeblake@users.noreply.github.com>
2023-12-03 11:55:33 +0900

    useAction; Form Components

--- app/(platform)/(dashboard)/organization/[organizationId]/form.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/form.tsx
@@ -1,22 +1,43 @@
 "use client"
 
-import { create } from "@/actions/create-board"
-import { Button } from "@/components/ui/button"
-import { useFormState } from "react-dom"
-import { FormInput } from "./form-input"
-import { FormButton } from "./form-button"
+import { createBoard } from "@/actions/create-board";
+import { FormInput } from "@/components/form/form-input";
+import { useAction } from "@/hooks/use-action";
+
+import { FormSubmit } from "@/components/form/form-submit";
 
 export const Form = () => {
-const initialState = { message: '', errors: {} };
-const [state, dispatch] = useFormState(create, initialState);
+    const { execute, fieldErrors } = useAction(createBoard, {
+        onSuccess: (data) => {
+            console.log(data, "SUCCESS!")
+        },
+        onError: (error) => {
+            console.log(error);
+        }
+    });
+
+    const onSubmit = (formData: FormData) => {
+        const title = formData.get("title") as string;
+
+        console.log({ title }); 
+
+        execute({ title });
+    }
 
     return (
-    <form action={dispatch}>
+    <form action={onSubmit}>
         <div className="flex flex-col space-y-2">
-            <FormInput errors={state?.errors}/>
+            <FormInput 
+                id="title"
+                label="Board Title"
+                errors={fieldErrors}
+            />
+
         </div>
         
-        <FormButton />
+        <FormSubmit>
+            Save
+        </FormSubmit>
     </form>
     )
 }
