55ea8a2babc1686bbcc726b6cd9eea19034dc4d3
langeblake <114412977+langeblake@users.noreply.github.com>
2023-12-04 23:41:17 +0900

    Board Server Action

--- app/(platform)/(dashboard)/_components/navbar.tsx
+++ app/(platform)/(dashboard)/_components/navbar.tsx
@@ -2,6 +2,7 @@ import { Plus } from "lucide-react";
 
 import { Logo } from "@/components/logo";
 import { Button } from "@/components/ui/button";
+import { FormPopover } from "@/components/form/form-popover";
 import { OrganizationSwitcher, UserButton } from "@clerk/nextjs";
 
 import { MobileSidebar } from "./mobile-sidebar";
@@ -14,12 +15,16 @@ const Navbar = () => {
             <div className="hidden md:flex">
                 <Logo />
             </div>
-            <Button variant="primary" size="sm" className="rounded-sm hidden md:block h-auto py-1.5 px-2">
-                Create
-            </Button>
+            <FormPopover align="start" side="bottom" sideOffset={18}>
+                <Button variant="primary" size="sm" className="rounded-sm hidden md:block h-auto py-1.5 px-2">
+                    Create
+                </Button>
+            </FormPopover>
+            <FormPopover>
             <Button variant="primary" size="sm" className="rounded-sm block md:hidden">
                 <Plus className="h-4 w-4"/>
             </Button>
+            </FormPopover>
         </div>
         <div className="ml-auto flex items-center gap-x-2">
             <OrganizationSwitcher 
--- app/(platform)/(dashboard)/organization/[organizationId]/_components/board-list.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/_components/board-list.tsx
@@ -1,6 +1,29 @@
-import { User2 } from "lucide-react"
+"use client";
+// Converted from server to client component to add Mobile State
+
+import { FormPopover } from "@/components/form/form-popover"
+import { Hint } from "@/components/hint"
+import { HelpCircle, User2 } from "lucide-react"
+import { useState, useEffect } from "react";
 
 export const BoardList = () => {
+    // Added state to handle board popover positioning for Mobile
+    const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setIsMobile(window.innerWidth < 768);
+        };
+
+        window.addEventListener('resize', handleResize);
+        handleResize();
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
+    
     return (
         <div className="space-y-4">
             <div className="flex items-center font-semibold text-lg text-neutral-700">
@@ -8,16 +31,28 @@ export const BoardList = () => {
                 Your boards
             </div>
             <div className="grid grid-cols-2 sm:g-cols-3 lg:grid-cols-4 gap-4">
-                <div
-                    role="button"
-                    className="aspect-video relative h-full w-full bg-muted rounded-sm flex flex-col gap-y-1 items-center justify-center hover:opacity-75 transition"
-                >
-                    <p className="text-sm">Create new board</p>
-                    <span className="text-xs">
-                        5 remaining
-                    </span>
-                </div>
+                {/* FormPopoever is the trigger to open children content*/}
+                <FormPopover sideOffset={10} side={isMobile ? "bottom" : "right"}> 
+                    <div
+                        role="button"
+                        className="aspect-video relative h-full w-full bg-muted rounded-sm flex flex-col gap-y-1 items-center justify-center hover:opacity-75 transition"
+                    >
+                        <p className="text-sm">Create new board</p>
+                        <span className="text-xs">
+                            5 remaining
+                        </span>
+                        <Hint
+                            sideOffset={40}
+                            side="bottom"
+                            description={`
+                                Free Workspaces can have up to 5 open boards. For unlimited boards, upgrade this workspace.
+                            `}
+                        >
+                            <HelpCircle className="absolute bottom-2 right-2 h-[14px] w-[14px]"/>
+                        </Hint>
+                    </div>
+                </FormPopover>
             </div>
-        </div>
+        </div> 
     )
 }
