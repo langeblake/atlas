db7e19b518f241b436ee4d729a56aea3e1892da6
langeblake <114412977+langeblake@users.noreply.github.com>
2024-01-10 22:51:45 +0900

    Landing Colors

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -39,7 +39,7 @@ const MarketingPage = () => {
             <h1 className="text-3xl md:text-6xl text-center text-neutral-800 mb-6">
                 Atlas is where plans
             </h1>
-            <div className="text-3xl md:text-6xl bg-gradient-to-r from-fuchsia-600 to-pink-600 text-white px-4 p-2 rounded-md pb-4 w-fit">
+            <div className="text-3xl md:text-6xl bg-gradient-to-r from-sky-600 to-indigo-600 text-white px-4 p-2 rounded-md pb-4 w-fit">
                 meet progress.
             </div>
         </div>
